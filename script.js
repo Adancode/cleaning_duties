ca1d5e3ce72e3b900aa8e56b22b37f46cab1560b
Adam Camacho <adamcamacho8@gmail.com>
2018-05-13 09:49:46 -0500

    cleaning app

---
+++ script.js
@@ -0,0 +1,90 @@
+console.log("connected");
+var duties = ["Wipe down UPstairs bathroom floor, sink, and toilet", "Spray tub & tub walls with anti-mildew spray and scrub down", "Sweep downstairs, mop specific spots if needed, don't mop everything (dangerous, slippery), only specific spots", "Wipe down DOWNstairs bathroom floor, sink, and toilet", "Make sure dishes don't pile up in sink and let me know who doesn't clean their own dishes", "Check dryer filter (every week) clean if needed, and check fridge for expired items, chuck if needed"];
+
+var tenants = ["Joshua Hawley", "David Sharp", "Benjamin Frei", "Jordan Green", "Imack Laidera", "Justin Sallese"];
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+// Used like so
+// dutiesShuffled = 
+shuffle(duties);
+shuffle(tenants);
+// console.log("the duties");
+// console.log(duties);
+// console.log(dutiesShuffled);
+// console.log(tenantsShuffled);
+
+//  An object holding all the duties
+// var monthlySchedule = {
+//     "Samuel Guebara": "Take trash out to curb on Sundays and back inside on Mondays"
+// }
+
+function resetDuties() {
+    $("tbody").empty();
+
+    shuffle(duties);
+    shuffle(tenants);
+    // for (var i = 0; i < tenants.length; i++) {
+    //     monthlySchedule[tenants[i]] = duties[i];
+    //     //$("tbody").append("<tr><td>" +  + "</td></tr>");
+    // }
+    duties.push("Take trash out to curb on Sundays and back inside on Mondays");
+    tenants.push("Samuel Guebara");
+
+    for (var i = 0; i < duties.length; i++) {
+        $("tbody").append("<tr><td class='firstTD'>" + tenants[i] + ":</td><td>" + duties[i] + "</td></tr>");
+    }
+
+    duties.pop();
+    tenants.pop();
+
+    // for (key in monthlySchedule) {
+    //     $("tbody").append("<tr><td class='firstTD'>" + key + ":</td><td>" + monthlySchedule[key] + "</td></tr>");
+    // }
+}
+
+$(document).ready(function () {
+
+    $("#resetMonthlyDutiesButton").on("click", function () {
+        resetDuties();
+    });
+
+    // for (var i = 0; i < tenants.length; i++) {
+    //     monthlySchedule[tenants[i]] = duties[i];
+    //     //$("tbody").append("<tr><td>" +  + "</td></tr>");
+    // }
+
+    duties.push("Take trash out to curb on Sundays and back inside on Mondays");
+    tenants.push("Samuel Guebara");
+
+    for (var i = 0; i < duties.length; i++) {
+        $("tbody").append("<tr><td class='firstTD'>" + tenants[i] + ":</td><td>" + duties[i] + "</td></tr>");
+    }
+
+    duties.pop();
+    tenants.pop();
+    // $("tbody").append("<tr>" + "hahah" + "</tr>");
+    // $("tbody").append("haha");
+});
+
+console.log(monthlySchedule);
+
+
+
