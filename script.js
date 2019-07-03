b42d5253b07240af88a7d4d1fcf78fcf14a5efc0
Adam Camacho <adamcamacho8@gmail.com>
2019-07-03 11:25:35 +0800

    updated tenants

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("connected");
 var duties = ["Wipe down UPstairs bathroom floor, sink, and toilet.  Empty that bathroom's trash if lid can't swing freely.", "Check downstairs & upstairs hallway air filters. Replace filters if needed (filters in pantry, install with arrow pointing in direction of airflow/into wall). Use small step ladder in pantry if needed.", "Sweep downstairs, mop specific spots if needed, don't mop everything (dangerous, slippery), only specific spots.", "Wipe down DOWNstairs bathroom floor, sink, and toilet.  Empty that bathroom's trash if lid can't swing freely.", "Make sure dishes don't pile up, and if possible, let Adan know if anyone doesn't clean their own dishes.  Wipe down kitchen sink and empty/clean kitchen sink filters (replace filters if needed, spare filters under sink).", "Check dryer filter (every week), wipe down stove/counters, and check fridges for expired items, chuck expired items."];
 
-var tenants = ["Joshua Hawley", "Jon Grogan", "Benjamin Frei", "James Duffy", "George Hernandez", "Justin Sallese"];
+var tenants = ["Joshua Hawley", "Yanji Chen", "Benjamin Frei", "James Duffy", "George Hernandez", "Justin Sallese"];
 
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -46,7 +46,7 @@ function resetDuties() {
     //     //$("tbody").append("<tr><td>" +  + "</td></tr>");
     // }
     duties.push("Take trash out to curb on Sundays and back inside on Mondays.");
-    tenants.push("Jay Robles");
+    tenants.push("Jon Grogan");
 
     for (var i = 0; i < duties.length; i++) {
         $("tbody").append("<tr><td class='firstTD'>" + tenants[i] + ":</td><td>" + duties[i] + "</td></tr>");
@@ -72,7 +72,7 @@ $(document).ready(function () {
     // }
 
     duties.push("Take trash out to curb on Sundays and back inside on Mondays");
-    tenants.push("Jay Robles");
+    tenants.push("Jon Grogan");
 
     for (var i = 0; i < duties.length; i++) {
         $("tbody").append("<tr><td class='firstTD'>" + tenants[i] + ":</td><td>" + duties[i] + "</td></tr>");
